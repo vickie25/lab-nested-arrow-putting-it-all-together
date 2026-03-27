941d4f13cf5f7fb3549d63adf7e66d6ec3389892
vickie25 <nyandorovictor3900@gmail.com>
2026-03-27 09:29:17 +0300

    downgrade jest from version 29.7.0 to 25.0.0 in devDependencies

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 
 
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+
+  return (passwordAttempt) => {
+    attemptCount++;
+    if (passwordAttempt === userInfo.password && attemptCount <= 3) {
+      return 'Login successful';
+    } else if (attemptCount > 3) {
+      return 'Account locked due to too many failed login attempts';
+    } else {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+  };
+}
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
